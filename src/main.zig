3c6decea24f78841725e2f0fe33640034bf5ea9f
Luke Harwood <lukeharwood11@gmail.com>
2024-09-30 18:28:42 -0500

    Clean up main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,4 @@
 const std = @import("std");
 const ll = @import("linkedlist.zig");
 
-pub fn main() !void {
-    // testing allocations
-    // why doesn't this work if I replace it with the GeneralPurposeAllocator?
-    const allocator = std.heap.page_allocator;
-    // defer _ = gpa.deinit();
-    // const allocator = gpa.allocator();
-    const mem = try allocator.alloc(i32, 2);
-    defer allocator.free(mem);
-    std.debug.print("{any}", .{allocator.resize(mem, 4)});
-}
+pub fn main() !void {}
