7b59d51e3929d408bb74e971ac73f3c374804b4f
= <=>
2024-09-29 19:02:01 -0500

    Add insert/get

--- src/linkedlist.zig
+++ src/linkedlist.zig
@@ -77,11 +77,39 @@ fn LinkedList(comptime T: type) type {
             return LinkedListError.IndexOutOfBounds;
         }
 
+        /// Inserts a value at the index given
+        /// If the index is out of bounds (> arr.len) a LinkedListError.IndexOutOfBounds is returned
+        /// This is an O(n) operation
+        fn insert(self: *Self, value: T, index: usize) !void {
+            var cur = self.head;
+            var idx: usize = 0;
+            // create our new node
+            const ptr = try self.allocator.create(Node);
+            ptr.value = value;
+            ptr.next = null;
+            // if something goes wrong (index out of bounds) destroy it
+            // if we don't do this, we'll have a memory leak
+            errdefer self.allocator.destroy(ptr);
+            while (cur) |node| : ({
+                cur = node.next;
+                idx += 1;
+            }) {
+                if (idx + 1 == index) {
+                    ptr.next = node.next;
+                    node.next = ptr;
+                    self.len += 1;
+                    break;
+                }
+            } else {
+                return LinkedListError.IndexOutOfBounds;
+            }
+        }
+
         /// Removes the first element from the list
         /// This is an O(1) operation
         fn pop(self: *Self) LinkedListError!T {
             if (self.head) |head| {
-                defer self.allocator.destroy(head);
+                defer self.allocator.destroy(head); // love this:)
                 self.head = head.next;
                 self.len -= 1;
                 return head.value;
@@ -155,4 +183,22 @@ test "Get" {
     try list.append(15);
     try std.testing.expect(try list.get(0) == 10);
     try std.testing.expect(try list.get(1) == 15);
+}
+
+test "Insert" {
+    const allocator = std.testing.allocator;
+    var list = LinkedList(i32).init(allocator);
+    defer list.deinit();
+
+    try std.testing.expectError(LinkedListError.IndexOutOfBounds, list.insert(10, 1));
+    try list.append(10);
+    try list.append(10);
+    try list.insert(50, 1);
+
+    var head = list.head.?;
+    try std.testing.expect(head.value == 10);
+    head = head.next.?;
+    try std.testing.expect(head.value == 50);
+    head = head.next.?;
+    try std.testing.expect(head.value == 10);
 }
